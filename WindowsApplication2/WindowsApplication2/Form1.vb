863fb5941df228e379b87678eb6443081b78c182
eslamelkhair <esotech3@gmail.com>
2023-03-30 17:53:31 +0200

    program to calculate numbers 1 to 100

---
+++ WindowsApplication2/WindowsApplication2/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim x As Integer
+        Dim sum As Integer
+
+        For x = 1 To 100
+            sum += x
+
+        Next
+
+        TextBox1.Text = sum
+    End Sub
+End Class
