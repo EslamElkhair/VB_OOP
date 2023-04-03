98d06c6d92631d8327d221e4e046f36e22657777
eslamelkhair <esotech3@gmail.com>
2023-04-04 01:56:57 +0200

    Dollars and Cents program

---
+++ WindowsApplication5/WindowsApplication5/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+  
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        Dim totalCents As Integer
+
+
+        totalCents = TextBox1.Text
+
+
+
+        Label3.Text = totalCents \ 100
+        Label4.Text = totalCents Mod 100
+
+
+
+    End Sub
+
+ 
+End Class
